126e78ce0d153494b00d2b9a3e2eb9a4a678d111
Caroline Tenório <caroltr.srs@gmail.com>
2015-11-22 23:51:30 -0200

    Arrumando fontes

--- app/src/main/java/postaround/tcc/inatel/br/Utils/PostAiTextView.java
+++ app/src/main/java/postaround/tcc/inatel/br/Utils/PostAiTextView.java
@@ -1,9 +1,13 @@
 package postaround.tcc.inatel.br.Utils;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
+
+import postaround.tcc.inatel.br.postaround.R;
 
 /**
  * Created by Carol on 13/11/2015.
@@ -12,6 +16,17 @@ public class PostAiTextView extends TextView {
 
     public PostAiTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/WalkwaySemiBold.ttf"));
+
+        int style = attrs.getAttributeIntValue("http://schemas.android.com/apk/res/android", "textStyle", Typeface.NORMAL);
+        int boldStyle = Typeface.BOLD;
+
+        Log.d("STYLE: ", style+"");
+
+        if (style == boldStyle) {
+            Log.d("STYLE BOLD","!");
+            this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/WalkwayRounded.ttf"));
+        } else {
+            this.setTypeface(Typeface.createFromAsset(context.getAssets(), "fonts/WalkwaySemiBold.ttf"));
+        }
     }
 }
